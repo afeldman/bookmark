71c0df41a95690226592536eef1340290092b1dc
anton feldmann <anton.feldmann@gmail.com>
2018-01-22 22:39:48 +0900

    question for url

--- lib/ruby/bookmark/html.rb
+++ lib/ruby/bookmark/html.rb
@@ -1,39 +1,79 @@
+#!/usr/bin/env ruby
+
+require_relative 'bookmark'
+
 require 'nokogiri'
 require 'open-uri'
 require 'date'
+require 'net/http'
 
-module Bookmark
+class NilClass
+  def nil_or_empty?
+    true
+  end
+end
 
-  class NilClass
-    def nil_or_empty?
-      true
-    end
+class Object
+  def blank?
+    respond_to?(:empty?) ? !!empty? : !self
   end
 
-  class String
-    def nil_or_empty?
-      empty?
-    end
+  # An object is present if it's not blank.
+  def present?
+    !blank?
+  end
+end
+
+class String
+  def nil_or_empty?
+    empty?
+  end
+end
+
+module Gmark
+
+  def self.url_exist?(url_string)
+    url = URI.parse(url_string)
+    req = Net::HTTP.new(url.host, url.port)
+    req.use_ssl = (url.scheme == 'https')
+    path = url.path if url.path.present?
+    res = req.request_head(path || '/')
+    res.code != "404" # false if returns 404 - not found
+  rescue Errno::ENOENT
+    false # false if can't find the server    
   end
   
   class BookmarkHTMLParser
-    def inizialize(url)
-      @doc = Nokogiri::HTML(open(url))
+    def initialize(url)
+      @url = url
+      if URL?
+        @doc = Nokogiri::HTML(open(url))
+      else
+        @doc = nil
+      end
     end
 
-    def getTitle
-      @doc.css('title')[0].text
+    def URL?
+      Gmark::url_exist?(@url)
     end
 
-    def self.getBookmark(url)
-      bk = BookmarkHTMLParser.new(url)
-      return { 'title' => (bk.getTitle.nil_or_empty? bk.getTitle : 'nt' ),
-               'date' => DateTime.now.strftime('%s'),
-               'url' => url,
-               'comment' => '',
-               'tags' => [] }
+    def doc?
+      return ( @doc? true : false)
     end
     
+    def getTitle
+      @doc.css('title')[0].text.strip if doc?
+    end
+
+    def getBookmark(parent_tag=nil)
+      Bookmark.new(@url,getTitle,parent_tag) if URL? and doc?
+    end
   end
-  
+end
+
+
+if $0 == __FILE__
+  url = 'https://rethinkdb.com/docs/permissions-and-accounts/'
+  html = Gmark::BookmarkHTMLParser.new(url)
+  p html.getBookmark
 end
